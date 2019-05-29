6efbba743427deb6143a84ec3a1ad07b301868c5
espinelie0049 <50212405+espinelie0049@users.noreply.github.com>
2019-05-29 10:02:25 -0700

    Create sizeofvariables.c

---
+++ cdemo/sizeofvariables.c
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+
+int main()
+{
+  int a = 545;
+  char b = 'h';
+  float c = 6.574;
+  double  d = 3.456789;
+
+
+  // print value and size of an int variable
+
+printf("My int is : %d, My float is : %f, My char is : %c\n", a, c, b);
+
+  printf("int a value: %d and size: %d bytes\n", a, sizeof(a));
+  printf("char b value: %c and size: %d bytes\n", b, sizeof(b));
+  printf("float c value: %f and size: %d bytes\n", c, sizeof(c));
+  /*printf("double d value: %d and size: %d bytes\n", d, sizeof(d));
+*/
+
+
+
+
+
+}
