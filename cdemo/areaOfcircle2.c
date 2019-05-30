717151a7ec6e06f4e5d63c49d88391364c533438
espinelie0049 <50212405+espinelie0049@users.noreply.github.com>
2019-05-30 09:23:51 -0700

    Create areaOfcircle2.c

---
+++ cdemo/areaOfcircle2.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <math.h>
+
+
+
+
+void areaofcircle(int r){
+
+float area = 3.14159*(r*r);
+
+printf("area is %f when radius is %d\n", area, r);
+
+
+}
+
+int main()
+{
+char input[256];
+float radius;
+
+printf("first radius: ");
+fgets(input, 256,stdin);
+sscanf(input, "%f", &radius);
+
+
+float radius2;
+
+printf("second radius: ");
+fgets(input, 256,stdin);
+sscanf(input, "%f", &radius2);
+
+
+
+
+areaofcircle(radius);
+areaofcircle(radius2);
+
+return 0;
+
+}
