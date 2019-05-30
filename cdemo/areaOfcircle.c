cd6c183737972c76d232469cbf0f3ad462e61f13
espinelie0049 <50212405+espinelie0049@users.noreply.github.com>
2019-05-30 09:36:02 -0700

    Update areaOfcircle.c

--- cdemo/areaOfcircle.c
+++ cdemo/areaOfcircle.c
@@ -1,9 +1,13 @@
+
 #include <stdio.h>
 #include <math.h>
 
+
+
+
 void areaofcircle(int r){
 
-float area = 3.14*(r*r);
+float area = 3.14159*(r*r);
 
 printf("area is %f when radius is %d\n", area, r);
 
@@ -13,14 +17,34 @@ printf("area is %f when radius is %d\n", area, r);
 int main()
 {
 
-for (int a = 3.5; a < 12.5; a++) {
+
+
+
+char input[256];
+float radius;
+
+printf("first radius: ");
+fgets(input, 256,stdin);
+sscanf(input, "%f", &radius);
+
+
+float radius2;
+
+printf("second radius: ");
+fgets(input, 256,stdin);
+sscanf(input, "%f", &radius2);
+
+for (int a = radius; a <= radius2; a++) {
 
 areaofcircle(a);
 
 }
 
+/*
+
+areaofcircle(radius);
+areaofcircle(radius2);
+*/
 return 0;
 
 }
-
-
